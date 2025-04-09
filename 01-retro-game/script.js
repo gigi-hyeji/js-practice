f9ff5e02d92567f7a8a09eb94532e1c411837ba0
gigi-hyeji <gigi.hyeji@gmail.com>
2025-04-08 23:27:09 -0700

    Manipulating CSS Styles

--- 01-retro-game/script.js
+++ 01-retro-game/script.js
@@ -18,10 +18,17 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number! 🚫';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!! 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -31,6 +38,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
@@ -40,6 +49,5 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-  } else {
   }
 });
