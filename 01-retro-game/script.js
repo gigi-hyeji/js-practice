a7ece03551aca24ee84b87d5ef9a6cc372190454
gigi-hyeji <gigi.hyeji@gmail.com>
2025-04-08 23:14:19 -0700

    set the score logic

--- 01-retro-game/script.js
+++ 01-retro-game/script.js
@@ -12,20 +12,34 @@ document.querySelector('.guess').value =
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20; //state variable
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
-
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number! 🚫';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!! 🎉';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--; //score = score - 1; 감소연산자
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else {
   }
 });
