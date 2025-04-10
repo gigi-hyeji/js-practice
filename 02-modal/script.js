bc7d373fdf96733a1411d519485dcf4ec3aaa76c
gigi-hyeji <gigi.hyeji@gmail.com>
2025-04-10 00:20:16 -0700

    Handling an esc keyress event

--- 02-modal/script.js
+++ 02-modal/script.js
@@ -6,17 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); //[button.show-modal, button.show-modal, button.show-modal]
 
 const openModal = function () {
-  console.log('Button Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-// modal.classList.remove('hidden');
-// overlay.classList.remove('hidden');
-// modal.style.display = 'block';
-
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log(`${i}button is clicked`);
+    openModal();
+  });
+  // modal.classList.remove('hidden');
+  // overlay.classList.remove('hidden');
+  // modal.style.display = 'block';
+}
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -33,4 +35,18 @@ overlay.addEventListener('click', closeModal);
 // overlay.addEventListener('click', function () {
 //   modal.classList.add('hidden');
 //   overlay.classList.add('hidden');
+// });
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// document.addEventListener('keydown', function (event) {
+//   if (event.key === 'Escape') {
+//     if (!modal.classList.contains('hidden')) {
+//       closeModal();
+//     }
+//   }
 // });
