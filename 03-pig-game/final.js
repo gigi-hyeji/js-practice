a800dd4b0cea8b276df06921fe269f62d4e8a0b9
gigi-hyeji <gigi.hyeji@gmail.com>
2025-04-15 02:32:19 -0700

    Include Pig Game final version

--- 03-pig-game/final.js
+++ 03-pig-game/final.js
@@ -13,14 +13,28 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   //switch to next player
@@ -80,14 +94,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // Activate the reset button
-document.querySelector('.btn--new').addEventListener('click', function () {
-  diceEl.classList.add('hidden');
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  document.querySelectorAll('.score').forEach((el) => (el.textContent = 0));
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-  playing = true;
-});
+document.querySelector('.btn--new').addEventListener('click', init);
